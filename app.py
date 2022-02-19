56e5129608e80223d767318843c58efb8bc9551f
NeeravWHJ <71086982+NeeravWHJ@users.noreply.github.com>
2022-02-19 10:32:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import csv 
+
+df = pd.read_csv("dwarf_stars.csv")
+print(df.shape)
+
+del df["Luminosity"]
+del df["Star_name"]
+del df["Distance"]
+del df["Mass"]
+del df["Radius"]
+
+df.to_csv("final.csv")
